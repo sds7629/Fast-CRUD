c5aa758ad8936c350d0835013ff1d57ec1fbc360
sds7629 <sds7629@naver.com>
2024-01-23 18:48:01 +0900

    Feat: FastAPI JWT Practice JWT 토큰

--- routers/user_routers.py
+++ routers/user_routers.py
@@ -1,7 +1,10 @@
 import os
+import datetime
+from jose import jwt, JWTError
+from datetime import timedelta
 from sqlalchemy.orm import Session
 from db.connection import get_db
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Response, Request
 from fastapi.security import OAuth2PasswordRequestForm
 from schema import user_schema
 from cruds import user_crud
@@ -14,6 +17,17 @@ ACCESS_TOKEN_EXPIRE_MINUTES = float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 app = APIRouter(prefix="/user")
+
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @app.post(path="/signup")
@@ -32,7 +46,9 @@ async def signup(new_user: user_schema.NewUserForm, db: Session = Depends(get_db
 
 @app.post(path="/login")
 async def login(
-    login_form: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)
+    response: Response,
+    login_form: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
 ):
     user = user_crud.get_user(login_form.username, db)
 
@@ -43,9 +59,30 @@ async def login(
 
     res = user_crud.verify_password(login_form.password, user.hashed_password)
 
+    access_token_expires = timedelta(MINUTE=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.user_name}, expires_delta=access_token_expires
+    )
+
+    response.set_cookie(
+        key="access_token",
+        value=access_token,
+        expires=access_token_expires,
+        httponly=True,
+    )
+
     if not res:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid user or password"
         )
 
-    return HTTPException(status_code=status.HTTP_200_OK, detail="Login Successful")
+    return user_schema.Token(access_token=access_token, token_type="bearer")
+
+
+@app.post("/logout")
+async def logout(response: Response, request: Request):
+    # access_token = request.cookies.get("access_token")
+    response.delete_cookie(key="access_token")
+    return HTTPException(
+        status_code=status.HTTP_204_NO_CONTENT, detail="Logout Successful"
+    )
